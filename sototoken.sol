7e42eec6e1bf4e28bbf292bb8c0cfb5a41971014
giyeolhankr7 <72843832+giyeolhankr7@users.noreply.github.com>
2021-09-10 14:09:23 +0900

    Update sototoken.sol

--- sototoken.sol
+++ sototoken.sol
@@ -90,7 +90,7 @@ contract StandardToken is Token {
     uint256 public totalSupply;
 }
 
-contract SotoCoin is StandardToken { // CHANGE THIS. Update the contract name.
+contract GYHCoin is StandardToken { // CHANGE THIS. Update the contract name.
 
     /* Public variables of the token */
 
@@ -110,12 +110,12 @@ contract SotoCoin is StandardToken { // CHANGE THIS. Update the contract name.
 
     // This is a constructor function 
     // which means the following function name has to match the contract name declared above
-    function SotoCoin() {
-        balances[msg.sender] = 1000000000000000000000;               // Give the creator all initial tokens. This is set to 1000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
-        totalSupply = 1000000000000000000000;                        // Update total supply (1000 for example) (CHANGE THIS)
-        name = "SotoCoin";                                   // Set the name for display purposes (CHANGE THIS)
+    function GYHCoin() {
+        balances[msg.sender] = 1000000000000000000000000;               // Give the creator all initial tokens. This is set to 1000,000 for example. If you want your initial tokens to be X and your decimal is 5, set this value to X * 100000. (CHANGE THIS)
+        totalSupply = 1000000000000000000000000;                        // Update total supply (1,000,000 for example) (CHANGE THIS)
+        name = "GYHCoin";                                   // Set the name for display purposes (CHANGE THIS)
         decimals = 18;                                               // Amount of decimals for display purposes (CHANGE THIS)
-        symbol = "SOTO";                                             // Set the symbol for display purposes (CHANGE THIS)
+        symbol = "GYH";                                             // Set the symbol for display purposes (CHANGE THIS)
         unitsOneEthCanBuy = 10;                                      // Set the price of your token for the ICO (CHANGE THIS)
         fundsWallet = msg.sender;                                    // The owner of the contract gets ETH
     }
